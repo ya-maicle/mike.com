ddbf372ba7cbbdb3e79c71f0067946f6e5908f44
ya-maicle <ya@maicle.co.uk>
2025-08-12 23:50:30 +0100

    chore(web): integrate shared configs and token styles

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,16 +1,5 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import config from '@maicle/config/eslint'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default [
+  ...config
+]
